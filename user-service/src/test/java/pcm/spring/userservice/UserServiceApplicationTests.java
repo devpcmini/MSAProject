d36c1ad6ad079e346735afabac860000be00dc52
devpcmini <devpcmini10@gmail.com>
2023-10-18 15:42:12 +0900

    서브모듈 연결 끊기

---
+++ user-service/src/test/java/pcm/spring/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pcm.spring.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
