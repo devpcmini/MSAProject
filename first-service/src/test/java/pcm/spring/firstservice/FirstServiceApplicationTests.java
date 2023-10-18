ecfcedca4a058695d1dd67479035c5191ea29071
devpcmini <devpcmini10@gmail.com>
2023-10-18 15:28:10 +0900

    MSA Proejct Commit

---
+++ first-service/src/test/java/pcm/spring/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pcm.spring.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
