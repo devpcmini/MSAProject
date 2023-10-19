d9ed876291ff88d2d4d4bd9bfdee3fdda17657c7
devpcmini <devpcmini10@gmail.com>
2023-10-19 15:39:58 +0900

    Spring Cloud Gateway - Load Balancer yml 파일에 포트번호 0으로 설정 후   instance:     instance-id: ${spring.application.name}:${spring.application.instance_id:${random.value}}를 통해 랜덤 포트 지정  -> apigateway에서 로드 밸런싱

--- first-service/src/main/java/pcm/spring/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/pcm/spring/firstservice/controller/FirstServiceController.java
@@ -1,16 +1,25 @@
 package pcm.spring.firstservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @Slf4j
 @RequestMapping("/first-service")
 public class FirstServiceController {
-	
+
+	Environment env;
+
+	public FirstServiceController(Environment env){
+		this.env = env;
+	}
+
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the First service.";
@@ -23,7 +32,8 @@ public class FirstServiceController {
 	}
 
 	@GetMapping("/check")
-	public String check(){
-		return "First Check";
+	public String check(HttpServletRequest request){
+		log.info("check server port => {}", request.getServerPort());
+		return String.format("port = %s",env.getProperty("local.server.port"));
 	}
 }
--- second-service/src/main/java/pcm/spring/secondservice/controller/SecondServiceController.java
+++ second-service/src/main/java/pcm/spring/secondservice/controller/SecondServiceController.java
@@ -1,16 +1,26 @@
 package pcm.spring.secondservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @Slf4j
 @RequestMapping("/second-service")
 public class SecondServiceController {
-	
+
+	Environment env;
+
+	public SecondServiceController(Environment env){
+		this.env = env;
+	}
+
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the Second service.";
@@ -23,7 +33,8 @@ public class SecondServiceController {
 	}
 
 	@GetMapping("/check")
-	public String check(){
-		return "Second Check";
+	public String check(HttpServletRequest request){
+		log.info("check server port => {}", request.getServerPort());
+		return String.format("port = %s",env.getProperty("local.server.port"));
 	}
 }
