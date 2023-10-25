01df988252fac3ff74eb5787edec28c445e26a79
devpcmini <devpcmini10@gmail.com>
2023-10-26 00:07:39 +0900

    ApiGateWay - hostname localhost를 설정하여 Failed to resolve '' after 4 queries 에러 해결

--- user-service/src/main/java/pcm/spring/userservice/controller/UserController.java
+++ user-service/src/main/java/pcm/spring/userservice/controller/UserController.java
@@ -1,24 +1,28 @@
 package pcm.spring.userservice.controller;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pcm.spring.userservice.dto.UserDto;
+import pcm.spring.userservice.service.UserService;
+import pcm.spring.userservice.vo.RequestUser;
 
 @RestController
 @RequestMapping("/user-service")
 public class UserController {
 
-//    private Environment env;
+    private Environment env;
+    private UserService userService;
 
-//    @Value("${greeting.message}")
-//    private String message;
-
-//    public UserController(Environment env){
-//        this.env = env;
-//    }
+    @Autowired
+    public UserController(Environment env, UserService userService){
+        this.env = env;
+        this.userService = userService;
+    }
 
     @GetMapping("/healthCheck")
     public String healthcheck(){
@@ -27,9 +31,18 @@ public class UserController {
 
     @GetMapping("/test")
     public String test(){
-//        return env.getProperty("greeting.message");
-//        return message;
-        return "test";
+        return env.getProperty("greeting.message");
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser requestUser){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(requestUser, UserDto.class);
+        userService.createUser(userDto);
+
+        return "Create User";
     }
 
 }
