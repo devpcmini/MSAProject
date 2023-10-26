4710b74596dee44ad5da6357056bd260d6ba0a85
devpcmini <devpcmini10@gmail.com>
2023-10-26 18:03:26 +0900

    User-service - Spring Security 추가

--- user-service/src/main/java/pcm/spring/userservice/UserServiceApplication.java
+++ user-service/src/main/java/pcm/spring/userservice/UserServiceApplication.java
@@ -1,8 +1,11 @@
 package pcm.spring.userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,8 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- user-service/src/main/java/pcm/spring/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/pcm/spring/userservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pcm.spring.userservice.dto.UserDto;
 import pcm.spring.userservice.jpa.UserEntity;
@@ -14,8 +15,16 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService{
 
-    @Autowired
     UserRepository userRepository;
+
+    BCryptPasswordEncoder bCryptEncoder;
+
+    @Autowired
+    public UserServiceImpl (UserRepository userRepository,
+                            BCryptPasswordEncoder bCryptEncoder){
+        this.userRepository = userRepository;
+        this.bCryptEncoder = bCryptEncoder;
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -24,8 +33,7 @@ public class UserServiceImpl implements UserService{
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd("encrtypted_password"); //임시값
-
+        userEntity.setEncryptedPwd(bCryptEncoder.encode(userDto.getPwd())); //임시값
         userRepository.save(userEntity);
 
         UserDto rtnUserDto = mapper.map(userEntity,UserDto.class);
