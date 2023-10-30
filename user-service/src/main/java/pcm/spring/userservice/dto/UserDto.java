810c2227406bc15550685124d7de2d0d2cac0bae
devpcmini <devpcmini10@gmail.com>
2023-10-30 23:25:24 +0900

    Users Microservice - 사용자 조회 기능 추가

--- user-service/src/main/java/pcm/spring/userservice/dto/UserDto.java
+++ user-service/src/main/java/pcm/spring/userservice/dto/UserDto.java
@@ -1,11 +1,13 @@
 package pcm.spring.userservice.dto;
 
 import lombok.Data;
+import pcm.spring.userservice.vo.ResponseOrder;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class UserDto {
@@ -16,4 +18,6 @@ public class UserDto {
     private Date createdAt;
 
     private String encryptedPwd;
+
+    private List<ResponseOrder> orders;
 }
--- user-service/src/main/java/pcm/spring/userservice/jpa/UserRepository.java
+++ user-service/src/main/java/pcm/spring/userservice/jpa/UserRepository.java
@@ -4,6 +4,5 @@ package pcm.spring.userservice.jpa;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
-
-
+    UserEntity findByUserId(String userId);
 }
