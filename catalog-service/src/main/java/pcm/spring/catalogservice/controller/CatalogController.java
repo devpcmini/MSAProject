582905e9456a2037c7ad244e08cc2e5d0451acb2
devpcmini <devpcmini10@gmail.com>
2023-11-08 22:46:21 +0900

    catalogService 안쓰는항목 제거

--- catalog-service/src/main/java/pcm/spring/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/pcm/spring/catalogservice/controller/CatalogController.java
@@ -1,15 +1,12 @@
 package pcm.spring.catalogservice.controller;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pcm.spring.catalogservice.jpa.CatalogEntity;
 import pcm.spring.catalogservice.service.CatalogService;
