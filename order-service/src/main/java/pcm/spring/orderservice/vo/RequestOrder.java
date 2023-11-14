d97a4820677f52b855a9c8e62e16e64d0b30caf1
devpcmini <devpcmini10@gmail.com>
2023-11-15 00:09:22 +0900

    Order Service RequestOrder 추가

---
+++ order-service/src/main/java/pcm/spring/orderservice/vo/RequestOrder.java
@@ -0,0 +1,14 @@
+package pcm.spring.orderservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RequestOrder {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+}
