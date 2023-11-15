1b188922c1f82348e2a79d470a25475219b92a2d
devpcmini <devpcmini10@gmail.com>
2023-11-15 23:33:50 +0900

    Order Service 안쓰는 Import 제거

--- order-service/src/main/java/pcm/spring/orderservice/vo/RequestOrder.java
+++ order-service/src/main/java/pcm/spring/orderservice/vo/RequestOrder.java
@@ -3,8 +3,6 @@ package pcm.spring.orderservice.vo;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RequestOrder {
