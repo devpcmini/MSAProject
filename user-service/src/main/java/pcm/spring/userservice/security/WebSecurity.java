5ba14225758d46e584c87095b09d1f5ce646d1a6
devpcmini <devpcmini10@gmail.com>
2023-11-20 00:00:12 +0900

    Users WebSecurity 수정  - 기존에 모든 요청에 대해 무조건 Permit All 했었던 부분 수정

--- user-service/src/main/java/pcm/spring/userservice/security/WebSecurity.java
+++ user-service/src/main/java/pcm/spring/userservice/security/WebSecurity.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -12,7 +14,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.45.206")
+                .and()
+                .addFilter(getAuthenticationonFilter());
+
         http.headers().frameOptions().disable(); //frame무시
+    }
+
+    private AuthenticationFilter getAuthenticationonFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
