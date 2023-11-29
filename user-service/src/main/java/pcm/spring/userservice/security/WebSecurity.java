c7e6583217dc685326e6a821f58cd752ff1e2283
devpcmini <devpcmini10@gmail.com>
2023-11-29 20:23:10 +0900

    Users WebSecurity 수정

--- user-service/src/main/java/pcm/spring/userservice/security/WebSecurity.java
+++ user-service/src/main/java/pcm/spring/userservice/security/WebSecurity.java
@@ -5,8 +5,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import javax.servlet.Filter;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
