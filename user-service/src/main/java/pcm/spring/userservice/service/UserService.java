3a4d3170e8712ca9c7ae947d95728197c1cd36e7
devpcmini <devpcmini10@gmail.com>
2023-12-17 21:38:30 +0900

    UserService  - getUserDetailByEmail 메서드 생성

--- user-service/src/main/java/pcm/spring/userservice/service/UserService.java
+++ user-service/src/main/java/pcm/spring/userservice/service/UserService.java
@@ -9,4 +9,6 @@ public interface UserService extends UserDetailsService {
 
     UserDto getUserByUserId(String userId);
     Iterable<UserEntity> getUserByAll();
+
+    UserDto getUserDetailByEmail(String email);
 }
